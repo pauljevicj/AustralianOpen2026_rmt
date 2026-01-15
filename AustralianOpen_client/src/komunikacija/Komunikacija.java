5a8dfd099d8c8a0536f19fd3d933ac2045925f6b
Jovana <pauljevicj12@gmail.com>
2026-01-15 17:22:30 +0100

    kostur projekta

---
+++ AustralianOpen_client/src/komunikacija/Komunikacija.java
@@ -0,0 +1,55 @@
+package komunikacija;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import transfer.KlijentskiZahtev;
+import transfer.ServerskiOdgovor;
+
+public class Komunikacija {
+    private Socket s;
+    private ObjectInputStream ois;
+    private ObjectOutputStream oos;
+    private static Komunikacija instance;
+    
+    private Komunikacija(){
+        try {
+            s = new Socket("localhost", 9000);
+            oos = new ObjectOutputStream(s.getOutputStream());
+            ois = new ObjectInputStream(s.getInputStream());
+        } catch (IOException e) {
+            Logger.getLogger(Komunikacija.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+    
+    public static Komunikacija getInstance() {
+        if (instance == null) 
+            instance = new Komunikacija();
+        return instance;
+    }
+    
+    public ServerskiOdgovor primiOdgovor(){
+        try {
+            //ois = new ObjectInputStream(s.getInputStream());
+            return (ServerskiOdgovor) ois.readObject();
+        } catch (IOException ex) {
+            Logger.getLogger(Komunikacija.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Komunikacija.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+    public void posaljiZahtev(KlijentskiZahtev kz){
+        try {
+            //oos = new ObjectOutputStream(s.getOutputStream());
+            oos.writeObject(kz);
+            oos.flush();
+        } catch (IOException ex) {
+            Logger.getLogger(Komunikacija.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
